22df15db2ccad630343964486055a6cf4d477039
Lucas-vdH <vanderhorst.lucas@gmail.com>
2024-06-30 17:59:14 +0200

    Modified popup content and style

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -4,6 +4,10 @@ window.onload = function() {
     document.getElementById('popup-box').classList.add('show');
 };
 
-document.getElementById('close-popup').onclick = function() {
+document.getElementById('close-popup1').onclick = function() {
+    document.getElementById('popup-box').classList.remove('show');
+};
+
+document.getElementById('close-popup2').onclick = function() {
     document.getElementById('popup-box').classList.remove('show');
 };
